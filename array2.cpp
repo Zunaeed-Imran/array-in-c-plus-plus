e7c6618b87f4623e1ec8d5259a77e481ddf00083
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-03-23 15:28:59 +0600

    sum of user input Value.

---
+++ array2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int sum = 0;
+  int n;
+  cout << "Enter n: ";
+  cin >> n;
+  int array [n];
+  for (int i = 0; i < n; i++) {
+    cout << "Enter " << i << ": ";
+    cin >> array [i];
+  }
+  for (int i = 0; i < n; i++) {
+    sum = sum + array [i];
+  }
+  cout << "sum is : " << sum;
+  return 0;
+}
