2f1d7df9d9a64279263cf72eb68a4796770ce4b1
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-04-13 10:38:31 +0600

    Update array3.cpp

--- array3.cpp
+++ array3.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main () {
   int a [3] [3] = {{5, 4, 1}, {2, 7, 9}, {1, 3, 8}};
-  for (int i = 0; i < 3; i++) {
+  for (int k = 0; k < 3; k++) {
     for (int j = 0; j < 3; j++) {
-      cout << a [i] [j] << " ";
+      cout << a [k] [j] << " ";
     }
     cout << endl;
   }
